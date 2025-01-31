03a12b1d4a8f88a3b733162a81ead5877e4b9f5d
datickhkikvadze <152197029+datickhkikvadze@users.noreply.github.com>
2025-01-31 16:54:28 +0400

    Add files via upload

---
+++ js/js day 1/app.js
@@ -0,0 +1,74 @@
+/*
+    ! primitive data types
+    -number
+    -string
+    -boolean
+    -undefined
+    -null
+    -symbol
+    -bigint
+
+        !variables
+        !   var, let, const
+
+        !ცვალადი არ შეიძლება დაიწყოს დიდი ალო-ბგერით
+        !ასევე არ იწყება სიმბოლოებით და რიცხვებით
+        !მეორე სიტყვეის პირველი ასო-ბგერა იწერება დიდი ასო-ბგერით ან ქვედა ტირით
+        !სახელის დარქმევის დროს არ გამოიყენება კოდური სიტყვები მაგ: for, class და ა.შ
+
+*/
+
+// console.log(2684848489)
+// console.log('hello world')
+// console.log("hello world")
+// console.log(true)
+// console.log(false)
+// console.log(null)
+// console.log(undefined)
+
+    // const firts_name = 'George'
+    // const firts_name2 = 'Ana'
+    // const age = true
+    // console.log(firts_name,firts_name2)
+
+    // console.log("wat's a beautiful day")
+    // console.log("I am" + ':' + ' ' + firts_name)
+    // console.log(`I am ${firts_name}`)
+    
+
+    
+    
+    // let first_name = "Dati"
+    // let last_name = "Cxikvadze"
+    // let age = 13
+    
+    // console.log("I am" + ':' + ' ' + first_name + ' ' +last_name + ' ' + 'and i am:' + ' ' + age +'yo')
+    // console.log(`I am: ${first_name} ${last_name} and i am: ${age} yo`)
+    
+    
+    // let num1 = 1
+    // let num2 = 2
+    // let num3 = 3
+    
+    // console.log(num1 + num2 + num3)
+    
+    
+    let first_name = "Dati"
+    let last_name = "Cxikvadze"
+    let num1 = 6
+    let num2 = 7
+    let age = num1 + num2
+    
+    console.log("I am" + ':' + ' ' + first_name + ' ' +last_name + ' ' + 'and i am:' + ' ' + age +'yo')
+    console.log(`I am: ${first_name} ${last_name} and i am: ${age} yo`)
+
+
+
+
+// let man = document.getElementById('man');
+// let clickCount = 0
+// man.addEventListener('click', () => {
+//     clickCount ++
+//     console.log('clicked:' + clickCount);
+//     man.textContent = 'clicked:' + clickCount
+// })
---
+++ js/js day 2/app.js
@@ -0,0 +1,38 @@
+// const name = 'gio'
+
+// const names = ['Davit','Cotne','Ana','Valera']
+
+// const mix_info = ['Anika', true, 46464, undefined, null]
+
+// console.log(mix_info[2]);
+
+const users = [ 
+    ['Davit',25 , ['white', 132]],
+    ['Cotne',27],
+    ['Ana', 26],
+    ['Valera', 23]
+]
+console.log(users[0][2][0]);
+
+
+
+
+
+// const my_mix_info = ['Daviti', 13, 'Georgian', 'white']
+
+// console.log(`my info : ${my_mix_info}`);
+
+// const phones = [ 
+//     ['Iphone 11', 2019, '64GB', '4GB Ram', 'white'],
+//     ['Iphone 12', 2020, '64GB', '4GB Ram', 'Black'],
+//     ['Iphone 13', 2021, '128GB', '4GB Ram', 'Gray']
+// ]
+
+// const Footballers = [ 
+//     ['Ronaldo', 1985, 10, '850 Goal', 'white'],
+//     ['Messi', 1987, 7, '218 GOals', 'white'],
+//     ['Kvara', 2001, 7, '921  Goal', 'white']
+// ]   
+
+// console.log(Footballers[0]);
+// console.log(phones[0]);
