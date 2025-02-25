18bad28c3152d8eb13d94c096cb1bd0a82653748
datickhkikvadze <152197029+datickhkikvadze@users.noreply.github.com>
2025-02-25 16:50:57 +0400

    Add files via upload

---
+++ e/app.js
@@ -0,0 +1,54 @@
+// const electronics = [
+//     {
+//         name: 'Phones',
+//         price: 1000,
+//         stock: 22
+//     },
+//     {
+//         name: 'Ipads',
+//         price: 1600,
+//         stock: 8
+//     },
+//     {
+//         name: 'Computers',
+//         price: 5000,
+//         stock: 2
+//     }
+// ]
+
+// let totalPrice = 0
+// let totalProducts = 0
+
+// for (let i = 0; i < electronics.length; i++) {
+//     totalPrice += electronics[i].price * electronics[i].stock
+//     totalProducts += electronics[i].stock
+// }
+
+
+// console.log('Total products:', totalProducts)
+// console.log('Total price of products:', totalPrice)
+
+
+
+// 90-100 kaia  95-100 granti
+
+const students = [
+    {
+        name: 'John',
+        grades: [80,56,94]
+    },
+    {
+        name: 'Sarah',
+        grades: [57,60,74]
+    },
+    {
+        name: 'Michael',
+        grades: [26,74,65]  
+    }
+]
+
+let sashualo = 0
+
+console.log(students[0].grades)
+
+
